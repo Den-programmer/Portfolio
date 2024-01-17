0dea8073aa7290b0f30ed474ec1f67970e7de1a3
Den-programmer <denisgrigorov094@gmail.com>
2024-01-17 19:48:18 +0100

    Updated information about portfolio

--- src/components/MainContent/Contact/contact.jsx
+++ src/components/MainContent/Contact/contact.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './contact.module.scss'
 import s from '../../common/LinkBtn/linkBtn.module.scss'
 import { NavLink } from 'react-router-dom'
 
 const Contact = (props) => {
+    const [isErrorMessage , setErrorMessageStatus] = useState(false)
+    const [errorMessage] = useState('The contact form isn\'t working for now. Because I need to do some backend for it')
     return (
         <div className={classes.contactContainer}>
             <h2 className={classes.inf__title}>Contact</h2>
@@ -16,8 +18,8 @@ const Contact = (props) => {
                     </p>
                     <ul className={classes.list}>
                         <li className={classes.listItem}>Email: <span className={classes.listItemMarked}>denisgrigorov094@gmail.com</span></li>
-                        <li className={classes.listItem}>LinkedIn: <span className={classes.listItemMarked}>DenIsReltih</span></li>
-                        <li className={classes.listItem}>Github: <span className={classes.listItemMarked}>Den-programmer</span></li>
+                        <li className={classes.listItem}>LinkedIn: <span className={classes.listItemMarked}><a className={classes.listItemMarkedLink} target="_blank" href="https://www.linkedin.com/in/denis-grigorov-047695293/">DenIsEnlightener</a></span></li>
+                        <li className={classes.listItem}>Github: <span className={classes.listItemMarked}><a className={classes.listItemMarkedLink} target="_blank" href="https://github.com/Den-programmer">Den-programmer</a></span></li>
                     </ul>
                     <p className={classes.listFoot}>
                         You can also use the contact form on this page.
@@ -37,7 +39,10 @@ const Contact = (props) => {
                         <p className={classes.textfield_title}>Message</p>
                         <textarea className={classes.textarea} placeholder="Enter your message..." type="text" />
                     </div>
-                    <NavLink to='/Home'
+                    {isErrorMessage && <div className={classes.errorMessage}>
+                        <p>{errorMessage}</p>
+                    </div>}
+                    <NavLink onClick={() => setErrorMessageStatus(true)} to="/Contact"
                         className={s.navPanel__btn}>
                         <span style={{ zIndex: '100' }}>Send message</span>
                     </NavLink>
